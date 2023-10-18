8500ca395887aa7752c86e33b56b78ab6bd64010
samuel73000 <50187969+samuel73000@users.noreply.github.com>
2023-10-18 17:28:54 +0200

    portfolio

--- src/page/Home/Home-Hero.jsx
+++ src/page/Home/Home-Hero.jsx
@@ -4,7 +4,7 @@ import"../../scss/Home-Hero.scss";
 function HomeHero(){
     return(
     <section className="container-home"> 
-    <img src={require("../../Data/gif-dev.gif")} className="gif-dev-home"></img>
+    <img src={require("../../Data/gif-dev.gif")} className="gif-dev-home" alt="gif d'un dev"></img>
      <h1 className="h1-home">HEY, I'M POUARD SAMUEL</h1>
      <p className="p-home">A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product</p>
      <a className="btn-home" href="#projects">PORJECTS</a>
