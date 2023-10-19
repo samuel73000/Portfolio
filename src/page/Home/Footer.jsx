40e4316cd573c209a73ca12979c0aeaba4edc19d
samuel73000 <50187969+samuel73000@users.noreply.github.com>
2023-10-19 15:20:07 +0200

    portfolio

--- src/page/Home/Footer.jsx
+++ src/page/Home/Footer.jsx
@@ -11,17 +11,29 @@ function Footer() {
           A Frontend focused Web Developer building the Frontend of Websites and
           Web Applications that leads to the success of the overall product
         </p>
-        </div>
+      </div>
       <div className="div-2-footer">
         <h2 className="title-2-footer">SOCIAL</h2>
-        <a href="https://www.linkedin.com/in/samuel-pouard-645672184/" className="icon-footer">
-        <FontAwesomeIcon icon={faLinkedin} size="2xl" />
+        <a
+          href="https://www.linkedin.com/in/samuel-pouard-645672184/"
+          target="_blank"
+          rel="noreferrer"
+          className="icon-footer"
+        >
+          <FontAwesomeIcon icon={faLinkedin} size="2xl" />
         </a>
-        <a href="https://github.com/samuel73000" className="icon-footer">
-        <FontAwesomeIcon icon={faGithub} size="2xl" />
+        <a
+          href="https://github.com/samuel73000"
+          target="_blank"
+          rel="noreferrer"
+          className="icon-footer"
+        >
+          <FontAwesomeIcon icon={faGithub} size="2xl" />
         </a>
       </div>
-      <p className="copyright-footer">© Copyright 2023. Made by Pouard Samuel</p>
+      <p className="copyright-footer">
+        © Copyright 2023. Made by Pouard Samuel
+      </p>
     </section>
   );
 }
