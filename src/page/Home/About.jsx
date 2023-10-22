f3662bf8fb82f525aff9f041a1540d9512ad0528
samuel73000 <50187969+samuel73000@users.noreply.github.com>
2023-10-22 20:00:24 +0200

    portfolio

--- src/page/Home/About.jsx
+++ src/page/Home/About.jsx
@@ -1,6 +1,14 @@
+import React from "react";
+import { useInView } from "react-intersection-observer";
 import "../../scss/About.scss";
 function About() {
-  
+  const [refKnow, inViewKnow] = useInView({
+    triggerOnce: true, 
+  });
+
+  const [refSkills, inViewSkills] = useInView({
+    triggerOnce: true, 
+  });
   return (
     <section className="container-about" id="about">
       <div className="container-me">
@@ -11,7 +19,10 @@ function About() {
           current skills mostly in terms of programming and technology
         </p>
       </div>
-      <div className="container-know">
+      <div
+        className={`container-know ${inViewKnow ? "animate" : ""}`}
+        ref={refKnow}
+      >
         <h3 className="title-know">Get to know me!</h3>
         <p className="p-know">
           I'm a Frontend Web Developer building the Front-end of Websites and
@@ -29,7 +40,10 @@ function About() {
           CONTACT
         </a>
       </div>
-      <div className="container-skills">
+      <div
+        className={`container-skills ${inViewSkills ? "animate" : ""}`}
+        ref={refSkills}
+      >
         <h3 className="title-skills">My Skills</h3>
         <div className="display-skills">
           <div className="skills">HTML</div>
